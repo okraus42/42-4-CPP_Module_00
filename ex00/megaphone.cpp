aea6fa5c8d828c37e5780760252c83148f5360a0
Ondřej Kraus <okraus42+github@pm.me>
2024-05-02 16:57:36 +0200

    start

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,3 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	l;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (argv[i])
+		{
+			while (argv[i][l])
+			{
+				std::cout << (char) toupper(argv[i][l]);
+					l++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
